755974399cad1363c6f4784ebe8339d513c52ca3
sphynxy <octodisk@gmail.com>
2021-05-24 21:09:46 -0500

    commit

--- games/realpoke/game.js
+++ games/realpoke/game.js
@@ -1,1 +1,18 @@
 console.log("test");
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText)
+            }
+        }
+    }
+}
+readTextFile("file:///game_data")
