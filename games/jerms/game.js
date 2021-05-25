967f05867de6fe299048d833c5de704af2086934
sphynxy <octodisk@gmail.com>
2021-05-25 13:56:37 -0500

    comi

---
+++ games/jerms/game.js
@@ -0,0 +1,89 @@
+r = ['Ed, Edd n Eddy', 'Bozo the clown', 'Jerma985', 'Anthony Wimblestein', 'Arthur Sweets', 'Gordon Ramsay', 'Jermacraft', 'Goldmen', 'Captain J.R. Spinkleworth', 'Chuck Shnozwell', "30's Milkman", 'Dr. Don and Dr. Dan', 'Judges', 'Invader', 'Alkynsbloom', 'Gabe Newell', '+2 and -2', 'Han Solo', 'Ashley', 'Bernie Sanders', 'Andrew Jackerry Jacked', 'Knowing', 'Green Screen', 'D56taFo98 Gr88787Bghfa', 'Diego Lobo', 'Agent', 'James Madison', 'Gordon Freeman', 'ILABI', 'Laffy', 'Glue Man', 'Henry', 'Bill Crazy', 'DNA FOOD', 'Candyman', 'Dimyan', 'Etalyx', 'Gorilla', 'Binary Man', 'JermaThing', 'Dr. Mosquito', 'Female Bandits', 'Jbumbum7722', 'Giant Enemies', 'Burgah Boy', 'John Cena', 'Giant Enemy Spider', 'Gullo and Georgeo', 'Beetlejuice', 'ClownDog', 'James Garfield', 'Garret Thompson', 'Carl Griffinsteed', 'Drak', 'Fat Cop', 'Button-Up Barry', 'Catboy Jerma', 'Benjamin Franklin', 'Free', 'Bob Anderson', 'Bovine Ernie', 'Garfieldthemondaykiller', 'Dr. Digestion', 'Comb Stream', 'Jigsaw', 'Internet', 'Gnot a Gnoblin', 'Astrology Bitch Jerma', 'David Bowie', 'Dick "Dastardly" Richard', 'Frank Pizza', 'DemoGuard (Spy Statue)', "Karl Griffinsteed's Great x18 Great Grandfather", 'Bilbo Baggins', 'Jerma Family Tree', 'Jermaodin', 'Harold Sludge', 'JermaWink', 'Earthworm Jim', 'Bottom Half of The Two Person Horse', 'Comedy Guy', 'Jerma Moon', 'Grandpa', 'Assistant', 'Barack Obama', 'Larry Targetwalmart', 'JoN', 'Hornets', "Al'Gorro", 'Darren', 'Denz de Bayle', 'Flanker', 'CannedRoth', 'Chuck E. Cheese', 'Coura"', 'Ghost Kid', 'Captain Cheez-Itz', 'JEX', 'DJ', 'Exoskeleton Bug Millennium Boy', 'John Marston', 'Car Valet', 'DSponge', 'Daffy', 'Bat Boy', 'Captain Humphries', 'Caramel Sweets', 'Jerma Earth', 'Bean Bag Sonic', 'Guard - Hitman (2016)', 'Fornite.Jimmy.2010', 'Jeremy Rinehart', 'Agent 47', 'Diana Madison', 'Dale', 'Dean Dingus', 'Doorverine', 'Juggles', 'Frogmin', 'Angry Guy with Machete 48', 'Gnome', 'Elvis Presley', 'Death', 'Enemy Helicopter pilot', 'Gerrard', 'Category:Demons', 'Handsome Harry', 'Jerma Solar System', 'Count Chocula', "Cap'n Crunch", 'Granny (Post-Apocalyptic)', 'Father Michael', 'Byeah (species)', 'B. F. Bugleberry', 'Easy Pete', 'Giant Mech', 'Henry (Kingdom Come)', 'Audience Member', 'Fru Fru', 'DMT Robot', 'Batman', "Al'Guru", 'Captain Cheez-It', 'Four Horsemen', 'Jerma (E3 2018)', 'Cdos', 'Boba Fett Chief', 'Hector Al-Fury', 'God', 'Charlie Cheese Nips', 'Demon Lord Zeraxos', 'GrillMasterXBBQ', 'Big Chili D', 'Demon Cow Gada', 'Kit Fisto', 'Jerma Raptor', 'Jeremiah', 'Boo', 'Harold Handerson (Unofficial name)', 'Dogman', 'Dr. Oetker', 'David', 'Friendly Skeleton Horse', 'Blordow', 'Anthony Banthony', 'Doping Dan', 'Adam Sandler', 'Cuddles and Polly Polite', 'Arthur Morgan', 'Gary the Fuck-Up', 'Gamesux', 'Jex Rex', 'Hugh Grant Hope', 'Jerry', 'Avril Lavigne', 'Isaac Kleiner', 'Cheers To You', 'Cat Police', 'IncredibleOrb', '5 Feet Off the Ground Police Helicopter', 'Jerma Matthew Kennedy', 'C', 'Danny DeVito', 'King Jerma CMLXXXV of Jermalonia', 'Jay Buffet', 'Easter Bunny', 'Coat Hanger', 'Fireman', 'Gary (Detective)', 'Garfield', 'Dell Inspiron 4100', 'Censored', 'I.R.I.S.']
+
+b = ['Jilstocham', 'Camemon', 'Barrh Mers Cilit Bamin', 'Anthas (End', 'Garne Sald Pocken', 'Jomra', 'Astord Macfa', 'Jarma Mals Grand Mapglo Binth', 'Jerman Horto', 'Asthon Harso', "An'le Suld", 'Demen', 'Cothont Jorma S',
+'N', 'Cungas', 'Gurd Margos', 'Gome Fip Stidt', 'Altand', 'Jenma', 'Alvh', 'Carn Manglim', 'Jomily Bametan', 'Grorde Farth', 'Cardy (onthar', 'Grorny', 'Bomo Mantamin', 'Cat',
+'Lasder', 'Boosy', 'Capele Barrant Bontamon', 'Damet', 'Ald', 'Gurrerd', 'Jorma Mont Grick Mamas Banthemy Ral', 'Jirma', 'Canset Jorson', 'Horrl Corn Grarn Chieg ',
+'Het Horga', 'Bapgard', 'Jarma Sort Gurfiis Ghesy (21201))', 'Bilat', 'Handad Mansh', 'Gronsy Sintton Horma', 'Althor', 'Cang ',
+'.', 'Andin Mame', 'Geary Pemise', 'Deman', 'Hapsy (2pema', 'Bobo Batth', 'Jirma Raptard Bomemy Halthon', 'Dr. Mot', 'Jims Cost', 'Dramis', 'Antrord Manthon Srrett', 'Gorma', 'Copsan',
+'6 Hargat', 'Herman', 'Borald Gaane Sandestin', 'Hanma Sargy Gorrlon', 'Grres Snd', 'Gornd Merga Mapgar', 'Born Fenta Fergat Botor', 'Gronden Hacker Sredton Greske Jerga Bogn', 'Corry Greet Grerlan', 'Dirga', 'Bergen', 'Garra Marser ',
+'Jerma Sing Muggson Halgson', 'Gareme Ban', 'Jorsat Bangy', 'Gareth', 'Berme', 'Baggl Guamd Sand', 'Jemmumin', 'Brthanen', 'Alta Ferr',
+'am Sardly Pere', 'Jerma Sardhy', 'Jirma Eandhor Sony (2ogy', 'Doard Ghech Swras Hale Bistard', 'Jomma Ranta', 'Aatd Meme Mersy', 'Dirga', 'Forne', 'Berra Bitk', 'Capan Checle', 'Asgre Bemthon',
+'Him Sorth Guardy (apthor Borsan', 'Gorran Fordor', 'Asthon', 'Cankly Palt', 'Jurmo', 'Earrhon Jomma', 'Banghos', 'Gurrl Saneme Famind', 'Grulta', 'Canklo Rop', 'Dirha', 'Cargy Bartar Momos', 'Jorra Rap', 'Jimrl Banta', 'Bamger', 'Corra',
+'Lam', 'Garat Cander Corghhon', 'Jerma Sarelo Parsen', 'Demantastard', 'Cand Morg Gront Horsy', 'Jarra', 'Ferne', 'Asvry Panta', 'Alat Bot Bor', 'Cantar Hopsos Grant Moma', 'Berre Burto Dimsos Gont Hemat Ghont', 'Jerma Earda',
+'07B10', 'Garmet Snrd Pamy', 'Gaand Cucz)', 'Goany Mals', 'Jemma Sapthoron', 'Barrly Humicy', 'Domenty Banghon', 'Gorme Eardint', 'Carrl', 'Careman (2p11) Sary Grant Grong Memeth Gumbon', 'Jorma Mapga Macfar', 'Dorny', 'Dormand', 'Andry B',
+'4F, Shnamon', 'Bagdat Fran Sarthir Hun', 'Deto Sat Chete', 'Jerma Runt Bomet', 'Dorot F Enestar Bossh', 'Corma Eerth', 'Garnet S2inth', 'Jerme Eilat Morser', 'Anthon Bar', 'Herra', 'Consy', 'Alen', 'Ganty', 'James Gar',
+'Hin', 'Harmashin', 'Jarma (andlos', 'Ganetton Hund Srdemon', 'Gronge', "As're", 'Furma Mactar Moptar Sont Jarsle', 'Jarrmo Sand Greclil', 'Gurdar', 'Alghor', 'Jarry', 'Jangan', 'Gamet Grent', 'Jemen Sankmon', 'Garne Ponge',
+'6 Guotd', 'Cang Mam Sort Bopemo', "Al'hon Horn Sady Parsan", 'Biggl Erumaton Grant Mos', 'Bet', 'Bor', 'Andho Jamsy', 'Agarta', 'Chuma Parta Bornandy', 'Gatalo', 'Lamile', 'Gront Coca', 'Jarty ',
+'s Grant Greme', 'Bovoldy Bangh', 'Corman', 'Digint', 'Goreld Sradta Camtaot Cheez-Ip', 'Garn Cachemert Bongh', 'Jomma', 'Durrh Frunkes', 'Harg Coles', 'D',
+'omon', 'Gardan Herdlon', 'Jorma Ransero', 'Jecma Sard Cup', 'Jerma Rarth', 'Cerma Sand Srralet', 'Jumma Sond Fomin Gandto', 'Denran Carth', 'Germa Eord Mersy Perky', "Al'Gront Canth", 'Germy Satke',
+'bo Falry', 'Jarra Perta Damro Slrest Specy', 'Farran', 'Garely Balico', 'Fargard', 'Graly', 'Anvras Snddlar', 'Jirran Falthon Bop Hotkes', 'Barrart', 'Anema Lagge Bargorson Hard Canthos', 'Gormo Marsa', 'Barta', 'Asthomen', 'Aggry', 'Garra',
+'w Jagma Rarth', 'Grrra Shne Pergan', 'Jerma Eanta', 'Cilg Memas', 'Agtandingow', 'Dr. Ootton Bargard Shrezs', 'Derma', 'Anthon', 'Jumma (L3 2 Dilly Bonkhy Horse', 'Barsy Bug',
+'vomon', 'Drvon Matger', 'Corgy Bamta', 'Carta Earth Cursatanhim', 'Jarma Ealt Jruntam Ghett', 'Corre', 'Cangler', 'Jarmans', 'Jarman', 'Horry Pargy', "Al'hurd", 'Dime',
+"An'ry Shranttomomos", 'Harge Bobry Sandler Horry Bon', 'Anamy', 'Bangar', 'Jimn Morton Gormin', 'Antron Gurd Shensem', 'Art',
+'qD', 'Gordat Hop', 'Jarma Eitkar', 'Jerma Mango', 'Barsit Chies', 'Gormy Mirker Mamily Burthory', "Al'Gury", 'Garry Eurthow Sand Mimit', 'Cardem Snresles', 'Damost Barich Enema', 'Bar']
+
+var points = 0;
+
+function game(iteration){
+
+    var pointP = document.getElementById('points');
+    var gDiv = document.getElementById('gameFrame');
+
+    pointP.innerHTML = "points " + points;
+    gDiv.innerHTML = "";
+
+    var cs = 0;
+
+    function check(side){
+
+        if(side == cs){
+            points += 1;
+        } else {
+            points = 0;
+        }
+        game(0);
+    }
+
+    function createButtons(a, b) {
+        var btn1 = document.createElement("button");
+        var btn2 = document.createElement("button");
+
+        btn1.innerHTML = a;
+        btn1.onclick = function() { check(0); };
+
+        btn2.innerHTML = b;
+        btn2.onclick = function() { check(1); };
+
+        gDiv.appendChild(btn1);
+        gDiv.appendChild(btn2);
+
+    }
+
+    const random1 = Math.floor(Math.random() * r.length);
+    const random2 = Math.floor(Math.random() * b.length);
+
+    if (Math.random() < 0.5){
+        cs = 0;
+        createButtons(r[random1], b[random2]);
+    } else {
+        cs = 1;
+        createButtons(b[random2], r[random1]);
+    }
+}
+
+function init(){
+
+   points = 0;
+
+   var gDiv = document.getElementById('gameFrame');
+   var btn = document.createElement("button");
+
+   btn.innerHTML = "start";
+   btn.onclick = function() { game(0) };
+   btn.id = "startb";
+
+   gDiv.appendChild(btn);
+
+}
+
+window.onload = init
